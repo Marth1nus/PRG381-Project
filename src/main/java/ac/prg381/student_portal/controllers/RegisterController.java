97138ee661b4853e69277bda0186495029986740
Marthinus <tinus1.prinsloo@gmail.com>
2023-08-28 22:44:49 +0200

    Boiler for services, repositories, and controllers

--- src/main/java/ac/prg381/student_portal/controllers/RegisterController.java
+++ src/main/java/ac/prg381/student_portal/controllers/RegisterController.java
@@ -1,8 +1,19 @@
 package ac.prg381.student_portal.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ac.prg381.student_portal.services.RegisterService;
+
 @RestController
+@RequestMapping("/register")
 public class RegisterController {
 
+  private final RegisterService registerService;
+
+  @Autowired
+  public RegisterController(RegisterService registerService) {
+    this.registerService = registerService;
+  }
 }
--- src/main/java/ac/prg381/student_portal/repositories/StudentRepository.java
+++ src/main/java/ac/prg381/student_portal/repositories/StudentRepository.java
@@ -2,6 +2,7 @@ package ac.prg381.student_portal.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import ac.prg381.student_portal.entities.Student;
 
 @Repository
