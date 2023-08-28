c1d607c5aefeb640dbff33509fb1b8024330d8e6
Marthinus <tinus1.prinsloo@gmail.com>
2023-08-28 17:48:20 +0200

    Initial project setup

---
+++ src/main/java/ac/prg381/studentportal/StudentportalApplication.java
@@ -0,0 +1,13 @@
+package ac.prg381.studentportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ac/prg381/studentportal/StudentportalApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.prg381.studentportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
