c007ce8097665865e510e25f7e0ad87ed270b3b5
Marthinus <tinus1.prinsloo@gmail.com>
2023-08-29 10:34:18 +0200

    Basic CRUD added to Service classes

--- src/main/java/ac/prg381/student_portal/services/AdministratorService.java
+++ src/main/java/ac/prg381/student_portal/services/AdministratorService.java
@@ -1,8 +1,11 @@
 package ac.prg381.student_portal.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ac.prg381.student_portal.entities.Administrator;
 import ac.prg381.student_portal.repositories.AdministratorRepository;
 
 @Service
@@ -15,6 +18,42 @@ public class AdministratorService {
     this.administratorRepository = administratorRepository;
   }
 
-  // CRUD
+  // Create
+
+  public Administrator addOrSetAdministrator(Administrator administrator) {
+    return administratorRepository.save(administrator);
+  }
+
+  public Administrator addAdministrator(Administrator administrator) {
+    Administrator existingAdministrator = administratorRepository.findById(administrator.getId()).orElse(null);
+    return existingAdministrator == null ? addOrSetAdministrator(administrator) : null;
+  }
+
+  // Read
+
+  public List<Administrator> getAllAdministrators() {
+    return administratorRepository.findAll();
+  }
+
+  public Administrator getAdministratorById(Long id) {
+    return administratorRepository.findById(id).orElse(null);
+  }
+
+  // Update
+
+  public Administrator setAdministrator(Administrator administrator) {
+    Administrator existingAdministrator = administratorRepository.findById(administrator.getId()).orElse(null);
+    return existingAdministrator != null ? addOrSetAdministrator(administrator) : null;
+  }
+
+  // Delete
+
+  public void removeAdministratorById(Long id) {
+    administratorRepository.deleteById(id);
+  }
+
+  public void removeAdministrator(Administrator administrator) {
+    removeAdministratorById(administrator.getId());
+  }
 
 }
--- src/main/java/ac/prg381/student_portal/services/RegisterService.java
+++ src/main/java/ac/prg381/student_portal/services/RegisterService.java
@@ -1,8 +1,11 @@
 package ac.prg381.student_portal.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ac.prg381.student_portal.entities.Register;
 import ac.prg381.student_portal.repositories.RegisterRepository;
 
 @Service
@@ -15,5 +18,42 @@ public class RegisterService {
     this.registerRepository = registerRepository;
   }
 
-  // CRUD
+  // Create
+
+  public Register addOrSetRegister(Register register) {
+    return registerRepository.save(register);
+  }
+
+  public Register addRegister(Register register) {
+    Register existingRegister = registerRepository.findById(register.getId()).orElse(null);
+    return existingRegister == null ? addOrSetRegister(register) : null;
+  }
+
+  // Read
+
+  public List<Register> getAllRegisters() {
+    return registerRepository.findAll();
+  }
+
+  public Register getRegisterById(Long id) {
+    return registerRepository.findById(id).orElse(null);
+  }
+
+  // Update
+
+  public Register setRegister(Register register) {
+    Register existingRegister = registerRepository.findById(register.getId()).orElse(null);
+    return existingRegister != null ? addOrSetRegister(register) : null;
+  }
+
+  // Delete
+
+  public void removeRegisterById(Long id) {
+    registerRepository.deleteById(id);
+  }
+
+  public void removeRegister(Register register) {
+    removeRegisterById(register.getId());
+  }
+
 }
