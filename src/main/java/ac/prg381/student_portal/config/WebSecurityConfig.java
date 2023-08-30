73f4458d7a010f885d06267bf03f3c2c694f84f6
Marthinus <tinus1.prinsloo@gmail.com>
2023-08-30 23:22:34 +0200

    Enabled method security i think?

--- src/main/java/ac/prg381/student_portal/config/WebSecurityConfig.java
+++ src/main/java/ac/prg381/student_portal/config/WebSecurityConfig.java
@@ -2,12 +2,14 @@ package ac.prg381.student_portal.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfig {
 
   @Bean
