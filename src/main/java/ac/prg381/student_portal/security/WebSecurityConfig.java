344a82760d615ad6a48413ff6762bd6abfcb01f2
Marthinus <tinus1.prinsloo@gmail.com>
2023-08-30 13:15:17 +0200

    Added unnamed indexes on .email

--- src/main/java/ac/prg381/student_portal/security/WebSecurityConfig.java
+++ src/main/java/ac/prg381/student_portal/security/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package ac.prg381.student_portal.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
